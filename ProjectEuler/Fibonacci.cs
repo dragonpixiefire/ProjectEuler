a2aaffdca8542758d1e320b5960dc18bccd38291
Miranda Dulin <dragonpixiefire@gmail.com>
2017-11-28 14:33:33 -0500

    Generate fibonacci within limit
    Need to be able to start anywhere in the sequence, and continue as long as a limit is not crossed per the problem statement.

--- ProjectEuler/Fibonacci.cs
+++ ProjectEuler/Fibonacci.cs
@@ -10,8 +10,24 @@ namespace ProjectEuler
         public static List<int> Generate(int firstSequence, int secondSequence, int limit)
         {
             var result = new List<int>();
+            if (firstSequence <= limit)
+            {
+                result.Add(firstSequence);
+            }
+            if (secondSequence <= limit)
+            {
+                result.Add(secondSequence);
+            }
+            if (result.Count == 2)
+            {
+                int sum = result[result.Count - 1] + result[result.Count - 2];
+                while (result[result.Count - 1] + result[result.Count - 2] <= limit)
+                {
+                    result.Add(sum);
 
-            
+                }
+            }
+
             return result;
         }
     }
--- ProjectEulerTests/FibonacciTests.cs
+++ ProjectEulerTests/FibonacciTests.cs
@@ -23,5 +23,56 @@ namespace ProjectEulerTests
 
         }
 
+        [Fact]
+        public void Generate_1Limit1_1()
+        {
+            //Arrange
+
+            int firstSequence = 1;
+            int secondSequence = 2;
+            int limit = 1;
+
+            //Act
+            var fibonacciResult = Fibonacci.Generate(firstSequence, secondSequence, limit);
+
+            //Assert
+            Assert.Equal(1, fibonacciResult[fibonacciResult.Count - 1]);
+
+        }
+
+        [Fact]
+        public void Generate_2Limit1_2()
+        {
+            //Arrange
+
+            int firstSequence = 1;
+            int secondSequence = 2;
+            int limit = 2;
+
+            //Act
+            var fibonacciResult = Fibonacci.Generate(firstSequence, secondSequence, limit);
+
+            //Assert
+            Assert.Equal(2, fibonacciResult[fibonacciResult.Count - 1]);
+
+        }
+
+        [Fact]
+        public void Generate_3Limit3_3()
+        {
+            //Arrange
+
+            int firstSequence = 1;
+            int secondSequence = 2;
+            int limit = 3;
+
+            //Act
+            var fibonacciResult = Fibonacci.Generate(firstSequence, secondSequence, limit);
+
+            //Assert
+            Assert.Equal(3, fibonacciResult[fibonacciResult.Count - 1]);
+
+        }
+
     }
 }
